f9949f1a7117269c51f82f110a4c21f59b781137
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-15 16:20:07 +0100

    Ciclo v-for in li

--- js/main.js
+++ js/main.js
@@ -4,10 +4,40 @@ const { createApp } = Vue;
 createApp({
     data(){
         return {
-            todoList : {
-                text: 'latte',
-                done: true
-            }
+            todoList : [
+                {
+                    "text": "Ripasso lezione",
+                    "done": true
+                },
+                {
+                    "text": "Creare repo",
+                    "done": true
+                },
+                {
+                    "text": "Fare esercizio",
+                    "done": false
+                },
+                {
+                    "text": "Caricare su GitHub",
+                    "done": false
+                },
+            ]
         }
     }
 }).mount('#app')
+
+// Descrizione:
+// Rifare l'esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// Bonus:
+// cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+// Buon lavoro e buon divertimento!
